1a3e44a886f9fe58e94bc39c964bc69b2a87e2a8
kolbeyang <kky2806@rit.edu>
2022-09-30 19:31:51 -0400

    Added stub for user class

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
@@ -0,0 +1,12 @@
+package com.estore.api.estoreapi.model;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * Represents a User
+ */
+public class User {
+}
