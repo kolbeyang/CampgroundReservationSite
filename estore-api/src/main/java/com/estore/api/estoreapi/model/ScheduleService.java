66bc095e5258d9af3904b2b32e9f07a8baec5ed6
kolbeyang <kky2806@rit.edu>
2022-09-30 19:08:49 -0400

    Reservations can be created with schedule checking.

--- estore-api/src/main/java/com/estore/api/estoreapi/model/ScheduleService.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/ScheduleService.java
@@ -7,23 +7,44 @@ import org.springframework.stereotype.Component;
 import com.estore.api.estoreapi.persistence.InventoryDAO;
 import com.estore.api.estoreapi.persistence.ReservationDAO;
 
+/**
+ * Handles Scheduling of Reservations
+ */
 @Component
 public class ScheduleService {
     private InventoryDAO inventoryDAO;
     private ReservationDAO reservationDAO;
 
+    /**
+     * Constructor
+     * @param inventoryDAO handles Campsite data
+     * @param reservationDAO handles Reservation data
+     */
     public ScheduleService(InventoryDAO inventoryDAO, ReservationDAO reservationDAO) {
         this.inventoryDAO = inventoryDAO;
         this.reservationDAO = reservationDAO;
     }
 
+    /**
+     * Determines if a is between b and c, exclusive
+     * @param a 
+     * @param b
+     * @param c
+     * @return true if b < a < c
+     */
     private boolean aBetweenBandC(long a, long b, long c) {
         // assumes that b < c
         boolean output = b < a && a < c;
-        System.out.println("aBetweenBandC for inputs " + a + " " + b + " " + c + " evaluated to " + output);
         return output;
     }
 
+    /**
+     * Determines whether the startDate and endDate conflict with any existing reservations on the campsite
+     * @param startDate the beginning of the reservation in milliseconds since 1970
+     * @param endDate the end of the reservation in milliseconds since 1970
+     * @param reservations the arraylist of reservation IDs from a campsite
+     * @return true if there are no schedule conflicts
+     */
     private boolean fitsInSchedule(long startDate, long endDate, ArrayList<Integer> reservations )  {
         Reservation currentReservation;
         long compareStart;
@@ -58,6 +79,13 @@ public class ScheduleService {
         return true;
     }
 
+    /**
+     * Checks to see if the reservation data will make a reservation data
+     * the campsiteId must exist and the start and end dates must fit within the campsite's schedule
+     * @param reservation the JSON data made into a reservation object
+     * @return whether the data will make a valid reservation
+     * @throws IOException 
+     */
     public boolean isValidReservation(Reservation reservation) throws IOException{
         System.out.println("ScheduleService: checking validity");
         int campsiteId = reservation.getCampsiteId();
@@ -67,9 +95,16 @@ public class ScheduleService {
             long endDate = reservation.getEndDate();
             return fitsInSchedule(startDate, endDate, campsite.getReservations());
         } 
+        System.out.println("ScheduleService.isValidReservation: Campsite of id " + campsiteId + " not found");
         return false;
     } 
 
+    /**
+     * Creates the reservation based on the data. Adds the reservation id to the campsite's arrayList of reservations
+     * @param reservation the JSON data made into a reservation object
+     * @return the crated reservation
+     * @throws IOException 
+     */
     public Reservation createReservation(Reservation reservation) throws IOException {
         Reservation created =  reservationDAO.createReservation(reservation);
         int campsiteId = reservation.getCampsiteId();
