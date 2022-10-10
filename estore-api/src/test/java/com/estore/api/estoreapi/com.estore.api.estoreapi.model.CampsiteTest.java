5c8de34b187232a154306312a2bbf78a14164ebe
Troy Wolf <wolftroy99@yahoo.com>
2022-10-10 19:06:42 -0400

    Campsite Test file

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/com.estore.api.estoreapi.model.CampsiteTest.java
@@ -0,0 +1,19 @@
+package com.estore.api.estoreapi;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("Model-tier")
+class comestoreapiestoreapimodelCampsiteTest{
+
+    @BeforeAll
+    public void setup() {
+        
+    }
+
+    
+    
+}
