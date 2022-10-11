b32ad7837b665794da153485a70ee3ac1b0e778b
Michael Oldziej <mo3047@rit.edu>
2022-10-11 00:15:57 -0400

    Worked on some of the tests in the first persistence unit test

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
@@ -12,6 +12,9 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.estore.api.estoreapi.model.*;
@@ -20,6 +23,70 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+
+
+/**
+ * Tests the Inventory File DAO class
+ */
 public class InventoryFileDAOTest {
+    InventoryFileDAO inventoryFileDAO;
+    ObjectMapper mockObjectMapper;
+    Campsite[] testCampsites;  
     
+
+
+    /**
+     * Instantiates the value for Inventory File DAO class tests so that each one will be tested
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupinventoryFileDAO()throws IOException{
+        mockObjectMapper = mock(ObjectMapper.class);
+        Campsite camp1 = new Campsite(1, "Lucky Meadows", 11.99);
+        Campsite camp2 = new Campsite(2, "Eagle heights", 12.43);
+        Campsite camp3 = new Campsite(3, "Stony Brook", 14);
+        testCampsites = new Campsite[3];
+        testCampsites[0] = camp1;
+        testCampsites[1] = camp2;
+        testCampsites[2] = camp3;
+
+
+            // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the hero array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"), Campsite[].class))
+                .thenReturn(testCampsites);
+
+        inventoryFileDAO = new InventoryFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+
+
+    
+    @Test
+    public void testgetCampsites() {
+        // Invoke
+        Campsite[] campsites = inventoryFileDAO.getCampsites();
+        System.out.println(campsites);
+        // Analyze
+        assertEquals(campsites.length,testCampsites.length);
+
+        for (int i = 0; i < campsites.length; i++)
+            assertEquals(campsites[0], testCampsites[0]);
+    }
+
+    @Test
+    public void testgetCampsite(){
+        Campsite[] campsites = inventoryFileDAO.getCampsites();
+       for(int i = 0; i < campsites.length; i++){
+            assertEquals(inventoryFileDAO.getCampsite(campsites[i].getId()), testCampsites[i]);
+       }
+    }
+
+    @Test
+    public void testfindCampsites(){
+        Campsite[] campsites  = inventoryFileDAO.getCampsites();
+
+    }
+
+
 }
