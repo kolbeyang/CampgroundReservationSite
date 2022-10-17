d37dfc3758be2a16cb79b465d7dc1d598640095c
Michael Oldziej <mo3047@rit.edu>
2022-10-16 21:10:11 -0400

    Fixed Admin and User View and changed the Admin Capabilities

--- E-store-Angular/src/app/product.service.ts
+++ E-store-Angular/src/app/product.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Product } from './Product';
-import { Observable, of } from 'rxjs';
 import { Campsite } from './Campsite';
-import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,9 @@ export class ProductService {
 
   private productUrl = 'http://localhost:8080/campsites'
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient) { }
@@ -19,6 +23,30 @@ export class ProductService {
     return this.http.get<Product[]>(this.productUrl)
   } 
 
+  updateProduct(product: Product): Observable<any> {
+    return this.http.put(this.productUrl, product, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${product.id}`)),
+      catchError(this.handleError<any>('updateCampsite'))
+    );
+    
+  }
+
+  deleteProduct(id: number): Observable<Campsite> {
+    const url = `${this.productUrl}/${id}`;
+
+    return this.http.delete<Campsite>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Campsite>('deleteCampsite'))
+    );
+  }
+
+  
+  log(arg0: string): void {
+    throw new Error('Method not implemented.');
+  }
+  handleError<T>(arg0: string): (err: any, caught: Observable<Object>) => import("rxjs").ObservableInput<any> {
+    throw new Error('Method not implemented.');
+  }
   
 
   //addProduct(name: string, id: number, rate: number): Observable<Campsite>{
