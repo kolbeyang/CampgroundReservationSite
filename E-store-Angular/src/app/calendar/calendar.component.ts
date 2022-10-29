0fb1b49ea3cc5311ec5d826feefd11c6541cbb9f
kolbeyang <kky2806@rit.edu>
2022-10-28 21:54:27 -0400

    Calendar Progress

--- E-store-Angular/src/app/calendar/calendar.component.ts
+++ E-store-Angular/src/app/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Campsite } from '../Campsite';
+import { ReservationService } from '../reservation.service';
 
 @Component({
   selector: 'app-calendar',
@@ -8,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
 
   daysOfTheWeek = ["S", "M", "T", "W", "Th", "F", "Sa"];
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   week1: Array<Date>;
   week2: Array<Date>;
 
@@ -15,11 +18,16 @@ export class CalendarComponent implements OnInit {
   endDate?: Date;
 
   calendarStartDate = new Date();
+  calendarTitle: string = "";
 
-  constructor() {
+  @Input() campsite?: Campsite;
+
+  color = "#FF0000";
+
+  constructor( reservationService: ReservationService) {
     this.week1 = new Array<Date>(7);
     this.week2 = new Array<Date>(7);
-    this.setCalendarDatesArrays();  
+    this.updateCalendar();  
   }
 
   ngOnInit(): void {
@@ -29,9 +37,17 @@ export class CalendarComponent implements OnInit {
     return new Date(date.getTime() - date.getDay() * 24 * 60 * 60 * 1000 );
   }
 
-  setCalendarDatesArrays(): void {
+  showDate(date: Date): void {
+    this.calendarStartDate = this.getPreviousSunday(date);
+    this.updateCalendar();
+  }
+
+  updateCalendar(): void {
     let week1Date = this.getPreviousSunday(this.calendarStartDate);
     let week2Date = new Date(week1Date.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+    let firstDate = week1Date;
+    let lastDate = new Date(week1Date.getTime() + 13 * 24 * 60 * 60 * 1000);
 
     for (let index = 0; index < 7; index++) {
       this.week1[index] = week1Date;
@@ -40,21 +56,35 @@ export class CalendarComponent implements OnInit {
       week1Date = new Date(week1Date.getTime() + 24 * 60 * 60 * 1000);
       week2Date = new Date(week2Date.getTime() + 24 * 60 * 60 * 1000);
     }
+
+    this.calendarTitle = this.months[firstDate.getMonth()] + " " + firstDate.getDate() + " - " + this.months[lastDate.getMonth()] + " " + lastDate.getDate();
   }
 
   changePreviousWeek(): void {
     this.calendarStartDate = new Date(this.calendarStartDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-    this.setCalendarDatesArrays();
+    this.updateCalendar();
   }
 
   changeNextWeek(): void {
     this.calendarStartDate = new Date(this.calendarStartDate.getTime() + 7 * 24 * 60 * 60 * 1000);
-    this.setCalendarDatesArrays();
+    this.updateCalendar();
+  }
+
+  getColor(date: Date): string {
+    return "#FFFFFF";
   }
 
   setStartDate(date: string | null): void {
+    console.log("Setting Start Date");
     if (date === null) {return;}
-    this.startDate = new Date(date);
+    let newDate = new Date(date);
+    this.startDate = newDate;
+    //if the start date is not shown, show it
+    console.log("calendarStartDate " + this.calendarStartDate.getTime() + " newDate " + newDate.getTime() + " 2 weeks "  + this.calendarStartDate.getTime() + 14 * 24 * 60 * 60 * 1000)
+    if (!(this.calendarStartDate.getTime() <= newDate.getTime() && newDate.getTime() <= this.calendarStartDate.getTime() + 14 * 24 * 60 * 60 * 1000)) {
+      console.log("showwing date");
+      this.showDate(newDate);
+    }
   } 
   setEndDate(date: string): void {
     if (date === null) {return;}
