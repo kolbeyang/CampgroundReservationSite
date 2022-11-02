fbb1e6cc23821d2308c4f60321f8e6d9184692f4
Michael Oldziej <mo3047@rit.edu>
2022-11-02 18:16:33 -0400

    Patched bug using *ngIf to distinquish between logged vs unlogged user

--- E-store-Angular/src/app/home-page/home-page.component.ts
+++ E-store-Angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private loginService: LoginService) { }
 
   ngOnInit(): void {
+
+  }
+
+  isLoggedIn(): boolean{
+    return this.loginService.isLoggedIn();
+  }
+  
+  isAdmin(): boolean{
+    return this.loginService.adminLoggedIn();
   }
 
 }
